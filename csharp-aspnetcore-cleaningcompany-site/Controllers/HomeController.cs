aa26c39998d56cc67db9da1e1d8a56d46dd8f417
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-03-23 21:29:27 +0200

    Добавьте файлы проекта.

---
+++ csharp-aspnetcore-cleaningcompany-site/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace csharp_aspnetcore_cleaningcompany_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
---
+++ csharp-aspnetcore-cleaningcompany-site/Program.cs
@@ -0,0 +1,10 @@
+namespace csharp_aspnetcore_cleaningcompany_site
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}
