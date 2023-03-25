b7f37d2fa104b47c196e9019120f514da23181c2
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-03-25 02:00:17 +0200

    setup: Intalling required NuGet packages.

--- csharp-aspnetcore-cleaningcompany-site/Program.cs
+++ csharp-aspnetcore-cleaningcompany-site/Program.cs
@@ -6,6 +6,8 @@ namespace csharp_aspnetcore_cleaningcompany_site
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
