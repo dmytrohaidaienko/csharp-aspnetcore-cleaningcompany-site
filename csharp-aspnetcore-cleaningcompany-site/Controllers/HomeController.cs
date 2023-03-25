f0ba718220f8d3b15f4eca87425f079bf13126f7
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-03-25 05:12:10 +0200

    chore: Created method for adding requests to the database.

--- csharp-aspnetcore-cleaningcompany-site/Controllers/HomeController.cs
+++ csharp-aspnetcore-cleaningcompany-site/Controllers/HomeController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using csharp_aspnetcore_cleaningcompany_site.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace csharp_aspnetcore_cleaningcompany_site.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationContext database;
+
+        public HomeController(ApplicationContext context)
+        {
+            database = context;
+        }
+
         public IActionResult Index() => View();
 
         public IActionResult About() => View();
@@ -11,5 +19,13 @@ namespace csharp_aspnetcore_cleaningcompany_site.Controllers
         public IActionResult Services() => View();
 
         public IActionResult Contact() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CustomerRequest customersRequest)
+        {
+            database.CustomersRequests.Add(customersRequest);
+            await database.SaveChangesAsync();
+            return RedirectToAction("Contact");
+        }
     }
 }
