9d7fcbfdd84d46fa3b3c53cdcc5e81256a6767a4
Dmytro Haidaienko <dmytrohaidaienko@gmail.com>
2023-06-02 03:00:26 +0300

    chore: Created calculator for calculate approximate cost of services in <Services.cshtml>.

---
+++ csharp-aspnetcore-cleaningcompany-site/wwwroot/js/calculator.js
@@ -0,0 +1,13 @@
+﻿function calculateCost() {
+    var areaInput = document.getElementById("area");
+    var area = areaInput.value;
+
+    if (area === "") {
+        alert("Пожалуйста, введите площадь.");
+        return;
+    }
+
+    var cost = area * 20;
+    var resultElement = document.getElementById("calculator-result");
+    resultElement.textContent = cost + " UAH";
+}
